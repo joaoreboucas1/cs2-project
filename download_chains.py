92196b65402478762a5d414e5895755dda31bcf3
joaoreboucas1 <joao.reboucas1@gmail.com>
2025-11-12 16:29:53 -0300

    Analyzing latest batch of chains with LSS observables

--- download_chains.py
+++ download_chains.py
@@ -3,14 +3,13 @@ import sys
 import subprocess
 
 # scp -r -P 13900 joaoreboucas@che.cbpf.br:~/cocoa/Cocoa/projects/cs2-project/chains/ .
-PATH="~/cocoa/Cocoa/projects/cs2-project/chains/"
-PATH_TEST="~/cocoa2/Cocoa/projects/des_y3/chains/"
+PATH="~/cocoa2/Cocoa/projects/cs2_project/chains/"
 if __name__ == "__main__":
     match len(sys.argv):
         case 1:
             print("Downloading all chains...")
             proc = subprocess.run([
-                "scp", "-r", "-P", "13900", f"joaoreboucas@che.cbpf.br:{PATH_TEST}", "./chains_test"
+                "scp", "-r", "-P", "13900", f"joaoreboucas@che.cbpf.br:{PATH}", "./chains/"
             ])
             if proc.returncode != 0: print("ERROR downloading all chains")
         case 2:
@@ -25,7 +24,7 @@ if __name__ == "__main__":
             print(f"Downloading chains {indices}")
             for i in indices:
                 proc = subprocess.run([
-                    "scp", "-r", "-P", "13900", f"joaoreboucas@che.cbpf.br:{PATH_TEST}/EXAMPLE_MCMC{i}*", "./chains_test/"
+                    "scp", "-r", "-P", "13900", f"joaoreboucas@che.cbpf.br:{PATH}/MCMC{i}", "./chains/"
                 ])
                 if proc.returncode != 0: print(f"ERROR downloading chain {i}")
         case default:
