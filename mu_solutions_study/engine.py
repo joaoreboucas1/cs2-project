41bf08d294a505aad02a9e51024147f32f7f457f
Joao Reboucas <joao.reboucas1@gmail.com>
2026-03-11 16:01:45 -0300

    Reworking the enging

--- mu_solutions_study/engine.py
+++ mu_solutions_study/engine.py
@@ -87,8 +87,9 @@ def get_alpha_K(aktype, a, bg, alpha_B, cs2, alpha_K_0):
 def deriv(loga, alpha_B, bg, aktype, alpha_K_0, cs2):
     a = np.exp(loga)
     wde, rhode, wtot, rhotot = get_bg_funcs(a, bg)
+    d_lnH_d_lna = -1.5*(1 + wtot)
     alpha_K = get_alpha_K(aktype, a, bg, alpha_B, cs2, alpha_K_0)
-    return cs2*(alpha_K + 1.5*alpha_B**2) + (alpha_B - 2)*(1.5*(1 + wtot) + 0.5*alpha_B) + 3*(1 + wtot) - 3*(1 + wde)*rhode
+    return cs2*(alpha_K + 1.5*alpha_B**2) + 0.5*alpha_B**2 - alpha_B*(d_lnH_d_lna + 1) - 3*(1 + wde)*rhode/rhotot
 
 def solve_alpha_B(aktype, omega_m, w0, wa, cs2, alpha_K_0, alpha_B_init=0):
     bg = Bg(omega_m=omega_m, w0=w0, wa=wa)
@@ -106,7 +107,10 @@ def solve_alpha_B(aktype, omega_m, w0, wa, cs2, alpha_K_0, alpha_B_init=0):
 
     alpha_B[0] = alpha_B_init
     alpha_K[0] = get_alpha_K(aktype, a[0], bg, alpha_B[0], cs2, alpha_K_0)
-    mu[0]      = 1 + alpha_B[0]**2/2/(alpha_K[0] + 1.5*alpha_B[0]**2)/cs2
+    D_kin      = alpha_K[0] + 1.5*alpha_B[0]**2
+    if alpha_B[0] == 0: mu[0] = 1
+    else:
+        mu[0]  = 1 + alpha_B[0]**2/(2*cs2*D_kin) if D_kin != 0 else np.inf
     for i in range(N):
         if abs(alpha_B[i]) > 1e6:
             # JVR NOTE: for many cases, \alpha_B just blows up and in Python it becomes \inf.
@@ -120,7 +124,10 @@ def solve_alpha_B(aktype, omega_m, w0, wa, cs2, alpha_K_0, alpha_B_init=0):
             break
         alpha_B[i+1] = alpha_B[i] + deriv(loga[i], alpha_B[i], bg, aktype, alpha_K_0, cs2)*dloga
         alpha_K[i+1] = get_alpha_K(aktype, a[i+1], bg, alpha_B[i+1], cs2, alpha_K_0)
-        mu[i+1]      = 1 + alpha_B[i+1]**2/2/(alpha_K[i+1] + 1.5*alpha_B[i+1]**2)/cs2
+        D_kin      = alpha_K[i+1] + 1.5*alpha_B[i+1]**2
+        if alpha_B[i+1] == 0: mu[i+1] = 1
+        else:
+            mu[i+1]  = 1 + alpha_B[i+1]**2/(2*cs2*D_kin) if D_kin != 0 else np.inf
         
     return loga*np.log10(np.e), alpha_B, alpha_K, mu
 
@@ -138,3 +145,61 @@ def study_alpha_B(alpha_K, cs2, omega_m, w0, wa):
         for j in range(N_alpha_B):
             result[i, j] = deriv(loga[i], alpha_B[j], alpha_K, cs2, omega_m, w0, wa)
     return result, loga*np.log10(np.e), alpha_B
+
+# ------------------------------------
+# Utility functions for model analysis
+# ------------------------------------
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+param_name_latex = {
+    "omega_m": "$\\Omega_m$",
+    "w0"     : "$w_0$",
+    "wa"     : "$w_a$",
+    "cs2"    : "$c_s^2$",
+    "alpha_K_0": "$\\alpha_{K,0}$",
+}
+
+def plot_scatter(aktype, priors, fig_path):
+    priors_keys  = list(priors.keys())
+    priors_array = np.array(list(priors.values()))
+    low = priors_array[:, 0]
+    high = priors_array[:, 1]
+
+    gr_threshold = 0.05
+    extreme_threshold = 0.5
+    
+    num_samples = 4_000
+    samples_params = np.random.rand(num_samples, len(priors)) * (high - low)[None, :] + low[None, :]
+    samples = []
+    for sample in samples_params:
+        params_dict = dict(zip(priors_keys, sample))
+        _, _, _, mu = solve_alpha_B(aktype=aktype, **params_dict)
+        if   np.any(mu < 0): color = "red" # I don't know if negative mu is a pathology but just in case
+        elif np.all(mu < 1 + gr_threshold) and np.all(mu > 1 - gr_threshold): color = "gray" # Mapping points close to GR
+        elif np.any(np.abs(mu - 1) > extreme_threshold): color = "orange" # Mapping regions with more extreme variation of mu
+        else: color = "blue" # "Typical" points
+        samples.append((sample, color))
+
+    fig, axs = plt.subplots(len(priors), len(priors), figsize=(12, 12), gridspec_kw={"hspace": 0, "wspace": 0})
+    points = np.array([sample[0] for sample in samples])
+    colors = np.array([sample[1] for sample in samples])
+    for row in range(len(priors)):
+        for col in range(len(priors)):
+            ax = axs[row, col]
+            if row == len(priors)-1: ax.set_xlabel(list(param_name_latex.values())[col])
+            else: ax.set_xticklabels([])
+            if col == 0:             ax.set_ylabel(list(param_name_latex.values())[row])
+            else: ax.set_yticklabels([])
+                
+            if col >= row: ax.remove()
+
+            ax.scatter(points[:, col], points[:, row], color=colors, s=1)
+
+    blue_circle = mpl.lines.Line2D([], [], marker="o", color="w", markerfacecolor="blue", label="$0.5 < \\mu < 1.5$ and", )
+    gray_circle = mpl.lines.Line2D([], [], marker="o", color="w", markerfacecolor="gray", label=f"$|\\mu - 1| < {gr_threshold}$", )
+    orange_circle = mpl.lines.Line2D([], [], marker="o", color="w", markerfacecolor="orange", label=f"$|\\mu - 1|$ above {extreme_threshold}", )
+    red_circle = mpl.lines.Line2D([], [], marker="o", color="w", markerfacecolor="red", label=f"$\\mu < 0$", )
+    fig.legend(handles=[gray_circle, blue_circle, orange_circle, red_circle], bbox_to_anchor=(0.75, 0.75))
+    plt.savefig(fig_path, bbox_inches="tight")
