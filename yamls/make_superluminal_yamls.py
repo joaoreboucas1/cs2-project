2ae748c2128ccb8c876ec0c13ed3231f7740c8f9
Joao Reboucas <joao.reboucas1@gmail.com>
2026-04-02 16:01:25 -0300

    Adding new yamls for superluminal + dynamical cs2

--- yamls/make_superluminal_yamls.py
+++ yamls/make_superluminal_yamls.py
@@ -1,6 +1,13 @@
-import os
+"""
+    Simple tool to change the cs2 prior in some yaml files and generate new yaml files
+"""
 
-subluminal_indices = range(56, 60)
+# subluminal_indices = range(56, 60) # This is for constant cs2
+# offset = 8 # NOTE: <subluminal chains>|<lcdm chains>|<superluminal chains>
+
+subluminal_indices = range(68, 72)
+offset = 4
+
 subluminal_cs2_prior_block = """cs2_0:
     prior:
       min: 0
@@ -13,7 +20,7 @@ superluminal_cs2_prior_block = """cs2_0:
 for i in subluminal_indices:
     subluminal_file = f"MCMC{i}.yaml"
     with open(subluminal_file, "r") as f: contents = f.read()
-    new_i = i + 8 # NOTE: <subluminal chains>|<lcdm chains>|<superluminal chains>
+    new_i = i + offset
     
     assert(f"MCMC{i}/MCMC{i}" in contents)
     contents = contents.replace(f"MCMC{i}/MCMC{i}", f"MCMC{new_i}/MCMC{new_i}")
