5151cdc31e073dad3859105bf634e6e69cfbad1d
Joao Reboucas <joao.reboucas1@gmail.com>
2026-04-08 18:05:52 -0300

    Updating chain tracker

--- report_chains.py
+++ report_chains.py
@@ -3,23 +3,15 @@
 """
 
 import os
+import time
 
 chain_indices = [folder[4:] for folder in os.listdir("./chains") if folder.startswith("MCMC") and os.path.isdir(f"./chains/{folder}")]
 for i in chain_indices:
-    # Check contents from MCMC{i}.progress
-    with open(f"./chains/MCMC{i}/MCMC{i}.progress", "r") as f: contents = f.read().splitlines()
-    last_status = contents[-1].split()
-    assert len(last_status) == 5, f"Last status should have 5 entries, found {last_status}"
-    num_samples, last_update, acc_rate, rminusone, rminusone_cl = last_status
-    print(f"MCMC{i} => num_samples = {round(float(num_samples))}, |R-1| = {rminusone} (updated {last_update})")
+    checkpoint_file_name = f"./chains/MCMC{i}/MCMC{i}.checkpoint"
     
-    # Check contents from cs2_{i}_{jobid}.out
-    logs = [log for log in os.listdir("./logs") if log.startswith(f"cs2_{i}") and log.endswith(".out")]
-    logs.sort(key=lambda log: int(log.strip(".out").split("_")[-1]))
-    last_log = logs[-1]
-    with open(f"./logs/{last_log}", "r") as f:
-        lines = f.read().splitlines()
-    for line in reversed(lines):
-        if "R-1" in line: break
-    print(f"Last update in {last_log}:", " ".join(line.split()[7:14]))
+    with open(checkpoint_file_name, "r") as f: contents = f.read().splitlines()
+    
+    last_R_minus_one = contents[3].split()[-1]
+    
+    print(f"MCMC{i} => |R-1| = {last_R_minus_one:.5f} (last update: {time.ctime(os.path.getmtime(checkpoint_file_name))})")
     print("-------")
