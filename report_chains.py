e3659c8266fc69f22aa05353aa9d639f36fe2bf9
Joao Reboucas <joao.reboucas1@gmail.com>
2026-04-08 18:09:53 -0300

    ANother tweak

--- report_chains.py
+++ report_chains.py
@@ -5,13 +5,13 @@
 import os
 import time
 
-chain_indices = [folder[4:] for folder in os.listdir("./chains") if folder.startswith("MCMC") and os.path.isdir(f"./chains/{folder}")]
+chain_indices = sorted([folder[4:] for folder in os.listdir("./chains") if folder.startswith("MCMC") and os.path.isdir(f"./chains/{folder}")])
+
 for i in chain_indices:
     checkpoint_file_name = f"./chains/MCMC{i}/MCMC{i}.checkpoint"
     
     with open(checkpoint_file_name, "r") as f: contents = f.read().splitlines()
     
-    last_R_minus_one = float(contents[3].split()[-1])
-    
-    print(f"MCMC{i} => |R-1| = {last_R_minus_one:.5f} (last update: {time.ctime(os.path.getmtime(checkpoint_file_name))})")
+    last_R_minus_one = contents[3].split()[-1]
+    print(f"MCMC{i} => |R-1| = {last_R_minus_one} (last update: {time.ctime(os.path.getmtime(checkpoint_file_name))})")
     print("-------")
