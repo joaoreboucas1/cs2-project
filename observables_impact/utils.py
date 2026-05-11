4f99bc30513a8ad9cd829d537655572e864b875e
Joao Reboucas <joao.reboucas1@gmail.com>
2026-05-11 17:59:22 -0300

    New plots

--- observables_impact/utils.py
+++ observables_impact/utils.py
@@ -25,7 +25,7 @@ def get_observables(case, H0, ombh2, omch2, As, ns, tau, w0, wa, dark_energy_typ
 
 def plot_pk_differences(cases, base_case, ks, redshifts, plot_name):
     # Precomputing min and max relative errors for unifying the colormaps in each axis
-    fig, axs = plt.subplots(len(cases), 1, sharex=True, sharey=True, figsize=(10, 7), gridspec_kw={"hspace": 0.11})
+    fig, axs = plt.subplots(len(cases), 1, sharex=True, sharey=True, figsize=(5, 7), gridspec_kw={"hspace": 0.11})
     scale = -np.inf
     for name, case in cases.items():
         relative_errors = 100*np.abs(case["P_lin"]/base_case["P_lin"] - 1)
@@ -39,18 +39,18 @@ def plot_pk_differences(cases, base_case, ks, redshifts, plot_name):
         mesh = ax.pcolormesh(ks, redshifts, relative_errors, cmap="seismic", norm=norm)
         ax.set_xscale('log')
         ax.set_ylabel(r"$z$", fontsize=20)
-        ax.text(0.83, 0.77, case["label"], transform=ax.transAxes, bbox={'boxstyle': 'round', 'facecolor': 'wheat', 'alpha': 0.5}, fontsize=20)
+        ax.text(0.97, 0.77, case["label"], transform=ax.transAxes, bbox={'boxstyle': 'round', 'facecolor': 'wheat', 'alpha': 0.5}, fontsize=20, ha="right")
         ax.set_yticks([0, 1, 2, 3])
 
     cbar = fig.colorbar(mesh, ax=axs, orientation="vertical", fraction=0.05, pad=0.02)
-    cbar.set_label(r"$100 \times \Delta P(k)/P(k)_\mathrm{GR}$", fontsize=20)
+    cbar.set_label(r"$100 \times \Delta P(k)/P(k)_{c_s^2=1}$", fontsize=20)
     cbar.ax.tick_params(labelsize=18)
     for ax in axs: ax.tick_params(labelsize=17)
     axs[-1].set_xlabel(r"$k \; (h/\mathrm{Mpc})$", fontsize=20)
-    plt.savefig(plot_name, bbox_inches="tight")
+    plt.savefig(plot_name, bbox_inches="tight", dpi=200)
 
 def plot_cmb_differences(cases, base_case, plot_name):
-    fig, axs = plt.subplots(2, 2, figsize=(15, 8), gridspec_kw={'wspace': 0.2, 'hspace': 0.2})
+    fig, axs = plt.subplots(2, 1, figsize=(7, 8), sharex=True, gridspec_kw={'wspace': 0.2, 'hspace': 0.2})
     ells_tt = np.arange(2051)
     ells_pp = np.arange(2001)
 
@@ -63,22 +63,22 @@ def plot_cmb_differences(cases, base_case, plot_name):
 
     for style, (name, case) in zip(styles, cases.items()):
         if name == "quint": continue
-        axs[0, 0].semilogx(ells_tt, 100*(case["cl_tt"]/base_case["cl_tt"] - 1), label=case["label"], **style)
-        axs[0, 1].semilogx(ells_pp, 100*(case["cl_pp"]/base_case["cl_pp"] - 1), label=case["label"], **style)
-        axs[1, 0].semilogx(ells_tt, 100*(case["cl_ee"]/base_case["cl_ee"] - 1), label=case["label"], **style)
-        axs[1, 1].semilogx(ells_tt, case["cl_te"] - base_case["cl_te"],   label=case["label"], **style)
+        axs[0].semilogx(ells_pp, 100*(case["cl_tt"]/base_case["cl_tt"] - 1)[:len(ells_pp)], label=case["label"], **style)
+        axs[1].semilogx(ells_pp, 100*(case["cl_pp"]/base_case["cl_pp"] - 1), label=case["label"], **style)
+        # axs[1, 0].semilogx(ells_tt, 100*(case["cl_ee"]/base_case["cl_ee"] - 1), label=case["label"], **style)
+        # axs[1, 1].semilogx(ells_tt, case["cl_te"] - base_case["cl_te"],   label=case["label"], **style)
 
-    axs[0, 0].set_xlabel(r"$\ell$", fontsize=15)
-    axs[0, 0].set_ylabel(r"$100 \times \Delta C_\ell^{TT}/C_{\ell, c_s^2=1}^{TT}$", fontsize=15)
-    axs[0, 1].set_xlabel(r"$\ell$", fontsize=15)
-    axs[0, 1].set_ylabel(r"$100 \times \Delta C_\ell^{\phi\phi}/C_{\ell, c_s^2=1}^{\phi\phi}$", fontsize=15)
-    axs[1, 0].set_xlabel(r"$\ell$", fontsize=15)
-    axs[1, 0].set_ylabel(r"$100 \times \Delta C_\ell^{EE}/C_{\ell, c_s^2=1}^{EE}$", fontsize=15)
-    axs[1, 1].set_xlabel(r"$\ell$", fontsize=15)
-    axs[1, 1].set_ylabel(r"$\Delta C_\ell^{TE}$", fontsize=15)
-    axs[0, 0].legend(fontsize=15, frameon=True, framealpha=1, edgecolor="black")
+    # axs[0].set_xlabel(r"$\ell$", fontsize=15)
+    axs[0].set_ylabel(r"$100 \times \Delta C_\ell^{TT}/C_{\ell, c_s^2=1}^{TT}$", fontsize=15)
+    axs[1].set_xlabel(r"$\ell$", fontsize=15)
+    axs[1].set_ylabel(r"$100 \times \Delta C_\ell^{\phi\phi}/C_{\ell, c_s^2=1}^{\phi\phi}$", fontsize=15)
+    # axs[1, 0].set_xlabel(r"$\ell$", fontsize=15)
+    # axs[1, 0].set_ylabel(r"$100 \times \Delta C_\ell^{EE}/C_{\ell, c_s^2=1}^{EE}$", fontsize=15)
+    # axs[1, 1].set_xlabel(r"$\ell$", fontsize=15)
+    # axs[1, 1].set_ylabel(r"$\Delta C_\ell^{TE}$", fontsize=15)
+    axs[0].legend(fontsize=15, frameon=True, framealpha=1, edgecolor="black")
     for ax in axs.flatten():
         ax.tick_params(axis='both', which='major', labelsize=13)
         ax.grid()
         ax.set_xlim([2, 2000])
-    plt.savefig(plot_name, bbox_inches="tight")
+    plt.savefig(plot_name, bbox_inches="tight", dpi=200)
