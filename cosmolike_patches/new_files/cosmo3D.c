0336819704a1fe095d8fd8da3541fe22910849fd
Joao Reboucas <joao.reboucas1@gmail.com>
2026-05-15 20:42:08 -0300

    Slight error in cosmo3D

--- cosmolike_patches/new_files/cosmo3D.c
+++ cosmolike_patches/new_files/cosmo3D.c
@@ -115,7 +115,7 @@ void get_mu_and_sigma(const double a_in, double *mu_out, double *sigma_out)
       const double a_hi     = cosmology.MGFuncs[0][i+1];
       const double mu_hi    = cosmology.MGFuncs[1][i+1];
       const double sigma_hi = cosmology.MGFuncs[2][i+1];
-      const double t = (a_in - a_low)/(a_hi - a_low);
+      double t = (a_in - a_low)/(a_hi - a_low);
       if (t > 1.0) t = 1.0;
       *mu_out    = mu_low + (mu_hi - mu_low)/(a_hi - a_low)*t;
       *sigma_out = sigma_low + (sigma_hi - sigma_low)/(a_hi - a_low)*t;
