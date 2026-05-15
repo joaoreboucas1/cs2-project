d78c46b52940b6074c83be2ad091026ca3d5ba9e
Joao Reboucas <joao.reboucas1@gmail.com>
2026-05-15 20:35:42 -0300

    Adding scripts to patch cosmolike

---
+++ cosmolike_patches/apply_patches.sh
@@ -0,0 +1,15 @@
+if [ -z ${ROOTDIR} ]; then
+    echo "ROOTDIR env variable not defined!"
+    exit 1
+fi
+
+SOURCE_DIR=${ROOTDIR}/projects/cs2-project/cosmolike_patches/new_files
+DES_Y3_DIR=${ROOTDIR}/projects/des_y3
+COSMOLIKE_DIR=${ROOTDIR}/external_modules/code/cosmolike
+
+cp ${SOURCE_DIR}/_cosmolike_prototype_base.py ${DES_Y3_DIR}/likelihood/
+cp ${SOURCE_DIR}/interface.cpp ${DES_Y3_DIR}/interface/
+cp ${SOURCE_DIR}/generic_interface.cpp ${COSMOLIKE_DIR}/
+cp ${SOURCE_DIR}/structs.h ${COSMOLIKE_DIR}/
+cp ${SOURCE_DIR}/cosmo2D.c ${COSMOLIKE_DIR}/
+cp ${SOURCE_DIR}/cosmo3D.c ${COSMOLIKE_DIR}/
